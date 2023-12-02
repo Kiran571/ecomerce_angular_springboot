495fb8fb9394ae45d1ed84814f3d26ee1c62488c
Kiran571 <kiranvsuryavanshi05@gmail.com>
2023-12-02 23:43:52 +0530

    2dec-util class started

--- src/main/java/com/ecomerce/angular/jwt/util/JwtUtil.java
+++ src/main/java/com/ecomerce/angular/jwt/util/JwtUtil.java
@@ -2,27 +2,45 @@ package com.ecomerce.angular.jwt.util;
 
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Component;
 
+import java.util.Date;
 import java.util.function.Function;
 
 @Component
 public class JwtUtil {
 
 
-    private  static final String SECRET_KEY = "DoWithReason";
-    public String getUserNameFromToken(String token){
+    String SECRET_KEY = "DoWithReason";
 
+    public String getUserNameFromToken(String token) {
+        return getClaimFromToken(token, Claims::getSubject);
     }
 
     //Method might need to accessible from diffwwernt methods
-    private <T> T getClaimFromToken(String token, Function<Claims,T>){
+    private <T> T getClaimFromToken(String token, Function<Claims, T> claimResolver) {
+        final Claims claims = getAllClaimsFromToken(token);
+        return claimResolver.apply(claims);
+    }
 
-
+    private Claims getAllClaimsFromToken(String token) {
+        return Jwts.parser().setSigningKey(SECRET_KEY).parseClaimsJws(token).getBody();
 
     }
 
-    private Claims getAllClaimsFromToken(String token){
-        return Jwts.parser().verifyWith(SECRET_KEY).build().parseSignedClaims(SECRET_KEY);
+    public boolean validateToken(String token, UserDetails userDetails) {
+        String userName = getUserNameFromToken(token);
+        return (userName.equals(userDetails.getUsername()) && !isTokenExpired(token));
+    }
+
+    //check token is expired or not
+    private boolean isTokenExpired(String token) {
+        Date expirationDate = getExpirationDateFromToken(token);
+        return expirationDate.before(new Date());
+    }
+
+    private Date getExpirationDateFromToken(String token) {
+        return getClaimFromToken(token, Claims::getExpiration);
     }
 }
